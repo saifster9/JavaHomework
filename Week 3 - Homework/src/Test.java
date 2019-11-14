f221521fd5326bd193dc9dc1e3587e8941475ee8
saifs <saifs@Osman-X1Ex2>
2019-11-13 23:08:17 -0600

    Week 3 - Homework - Test Class

---
+++ Week 3 - Homework/src/Test.java
@@ -0,0 +1,62 @@
+import java.text.DecimalFormat;
+import java.util.*;
+
+public class Test
+{
+	private int age;
+
+	/*
+	 * Write a method called cube that accepts one integer parameter and returns that value raised to the third power.
+	 
+	public double cube(int side)
+	{
+		double sidePowered = Math.pow(side, 3);
+		return sidePowered;
+	}
+*/
+	/*
+	 * Suppose you have a class called Movie. Write a constructor for the class that initializes the title and director 
+	 * instance variables based on parameters passed to the constructor.
+	 */
+/*	public movie(movieTitle, movieDirector, movieGenre)
+	{
+		this.title = movieTitle;
+		this.director = newDirector;
+	}
+	*/
+	public void Child()
+	{
+		age = 0;
+	}
+
+	public int getAge()
+	{
+		return age;
+	}
+
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+		
+		/*
+		Write code to declare and instantiate an object of the Random class (call the object reference variable rand). 
+		Then write a list of expressions using the nextInt method that generates random numbers in the following specified ranges, 
+		including the endpoints. Use the version of the nextInt method that accepts a single integer parameter.
+		0 to 10
+
+		0 to 400
+		
+		1 to 10
+		
+		1 to 400
+		
+		25 to 50
+		
+		-10 to 15
+		*/
+
+/*
+ * Write a method called lyrics that prints the lyrics of a song when invoked. The method should accept no parameters and return no value.
+*/ 
+}
